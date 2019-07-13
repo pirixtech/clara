3252de11c9046b8bf3d8f8542011476746b65d8a
Michael Hu <pxmyhdev@gmail.com>
2019-07-13 19:41:04 -0400

    add dialog flow

--- features/restaurant_features.js
+++ features/restaurant_features.js
@@ -1,6 +1,20 @@
+const { BotkitConversation } = require('botkit');
+
 module.exports = function(controller) {
   /* restarants */
-  controller.hears(['where to eat'], ['message'], async (bot, message) => {
+  controller.hears(['Where to eat'], ['message'], async (bot, message) => {
     await bot.reply(message, 'Pai is the best Thai restaurant in Toronto!');
+  });
+
+  let DIALOG_ID = 'restaurant_guide';
+  let restaurantDialog = new BotkitConversation(DIALOG_ID, controller);
+  // TODO: replace with location field parsed from message
+  let location = 'Toronto';
+  restaurantDialog.say('Hola!');
+  restaurantDialog.say('Welcome to ' + location + '!');
+  controller.addDialog(restaurantDialog);
+
+  controller.hears(["I'm hungry"], 'message', async (bot, message) => {
+    await bot.beginDialog(DIALOG_ID);
   });
 };
