c61e018409f5beb92f772a3812c0e7954f4ae6cf
Michael Hu <pxmyhdev@gmail.com>
2019-07-13 00:08:21 -0400

    add version info

---
+++ features/me.js
@@ -0,0 +1,23 @@
+module.exports = function(controller) {
+  /* bot app version */
+  controller.hears(
+    ['Who are you', 'Tell me about yourself'],
+    'message',
+    async (bot, message) => {
+      let incarnation = '0.1.0';
+      await bot.reply(
+        message,
+        "I'm the " + String(incarnation) + ' impossible girl'
+      );
+    }
+  );
+
+  function version() {
+    const rev = fs.readFileSync('.git/HEAD').toString();
+    if (rev.indexOf(':') === -1) {
+      return rev;
+    } else {
+      return fs.readFileSync('.git/' + rev.substring(5)).toString();
+    }
+  }
+};
