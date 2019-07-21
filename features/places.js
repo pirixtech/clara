864fe92e0db91db61488d496f173befabfec88ed
Michael Hu <pxmyhdev@gmail.com>
2019-07-21 14:17:53 -0400

    fix

--- features/places.js
+++ features/places.js
@@ -61,79 +61,64 @@ function lookupUserLocation() {}
 
 function fbPlacesTempl(sights, restaurants, experience) {
   // TODO: use a factory to produce the templates
-  let viewImageUrl =
+  var viewImageUrl =
     'https://www.toronto.ca/wp-content/uploads/2017/07/9163-invest-in-toronto-995x330.png';
-  let resaurantUrl =
+  var resaurantUrl =
     'https://www.blogto.com/eat_drink/2018/09/essential-restaurants-toronto/';
-  let sightsUrl =
+  var sightsUrl =
     'https://www.blogto.com/city/2015/08/the_10_most_breathtaking_views_of_toronto/';
-  let experiencesUrl =
+  var experiencesUrl =
     'https://www.blogto.com/city/2016/09/17_super_touristy_things_you_must_do_in_toronto/';
-  let fallbackUrl = 'https://www.google.com';
+  var fallbackUrl = 'https://www.blogto.com';
   return {
     type: 'template',
     payload: {
       template_type: 'generic',
       elements: [
         {
-          template_type: 'generic',
-          elements: [
+          title: 'Sights',
+          subtitle: `Toooooooooo many people, Please don't go`,
+          image_url: viewImageUrl,
+          buttons: [
             {
-              title: 'Sights',
-              subtitle: `Toooooooooo many people, Please don't go`,
-              image_url: viewImageUrl,
-              buttons: [
-                {
-                  title: 'View',
-                  type: 'web_url',
-                  url: sightsUrl,
-                  messenger_extensions: true,
-                  webview_height_ratio: 'full',
-                  fallback_url: fallbackUrl
-                }
-              ]
-            },
-            {
-              title: 'Restaurants',
-              subtitle: `Toooooooooo many people, Please don't go`,
-              image_url: viewImageUrl,
-              buttons: [
-                {
-                  title: 'View',
-                  type: 'web_url',
-                  url: resaurantUrl,
-                  messenger_extensions: true,
-                  webview_height_ratio: 'full',
-                  fallback_url: fallbackUrl
-                }
-              ]
-            },
-            {
-              title: 'Experiences',
-              subtitle: `Toooooooooo many people, Please don't go`,
-              image_url: viewImageUrl,
-              buttons: [
-                {
-                  title: 'View',
-                  type: 'web_url',
-                  url: experiencesUrl,
-                  messenger_extensions: true,
-                  webview_height_ratio: 'full',
-                  fallback_url: fallbackUrl
-                }
-              ]
+              title: 'View',
+              type: 'web_url',
+              url: sightsUrl,
+              messenger_extensions: true,
+              webview_height_ratio: 'full',
+              fallback_url: fallbackUrl
             }
           ]
         },
         {
-          title: "Too many fake reviews, don't go there",
-          subtitle: `Might as well check it out`,
-          default_action: {
-            type: 'web_url',
-            url: '',
-            messenger_extensions: false,
-            webview_height_ratio: 'tall'
-          }
+          title: 'Restaurants',
+          subtitle: `Toooooooooo many people, Please don't go`,
+          image_url: viewImageUrl,
+          buttons: [
+            {
+              title: 'View',
+              type: 'web_url',
+              url: resaurantUrl,
+              messenger_extensions: true,
+              webview_height_ratio: 'full',
+              fallback_url: fallbackUrl
+            }
+          ]
+        },
+        {
+          title: 'Experiences',
+          subtitle: `Toooooooooo many people, Please don't go`,
+          image_url: viewImageUrl,
+          buttons: [
+            {
+              title: 'View',
+              type: 'web_url',
+              url: experiencesUrl,
+              messenger_extensions: true,
+              webview_height_ratio: 'full',
+              fallback_url: fallbackUrl
+            }
+          ]
         }
       ]
     }
